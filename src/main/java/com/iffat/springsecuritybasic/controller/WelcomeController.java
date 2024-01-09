71642a916a4725a5fb9341611fd7b5f3d352b5d9
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-09 09:32:10 +0700

    01.Starter Project

---
+++ src/main/java/com/iffat/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.iffat.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayHello() {
+        return "Welcome to Spring Application with out Security";
+    }
+}
