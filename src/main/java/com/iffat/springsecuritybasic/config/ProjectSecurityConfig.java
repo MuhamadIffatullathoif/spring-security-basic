be1ad0046e550a31efaa5dadd56b952ae01c5508
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-11 10:33:48 +0700

    09.Configuring users using InMemoryUserDetailsManager

--- src/main/java/com/iffat/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/iffat/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,31 +19,30 @@ public class ProjectSecurityConfig {
          * Below is the custom security configuration
          */
 
-        /* http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                        .requestMatchers("/notices","/contact").permitAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-        return http.build(); */
-
-        /*
-         * Configuration to deny all the request
-         */
-
-        /* http.authorizeHttpRequests(auth -> auth
-                        .anyRequest().denyAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-        return http.build(); */
-
-        /*
-         * Configuration to permit all the request
-         */
-
         http.authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll())
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contact").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        /*
+         * Approach 1 where we use withDefaultPasswordEncoder() method
+         * While creating the user details
+         */
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
